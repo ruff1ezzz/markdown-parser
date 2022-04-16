87f1a35ac0e0b73d91fb59fa80eaa8dace260f38
ruff1ezzz <88177499+ruff1ezzz@users.noreply.github.com>
2022-04-15 17:47:47 -0700

    Error fixing broken file

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,9 +16,17 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+
+            if (markdown.substring(openParen + 1, closeParen).contains("(") 
+                || markdown.substring(openParen + 1, closeParen).contains(")")) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+
         }
 
         return toReturn;
