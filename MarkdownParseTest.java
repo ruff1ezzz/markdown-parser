8448667df04dac68b53f0a5e6c5a445ca1ccfb06
ruff1ezzz <88177499+ruff1ezzz@users.noreply.github.com>
2022-04-27 16:38:49 -0700

    Update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,12 @@
 import static org.junit.Assert.*;
-import org.junit.*;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
@@ -14,11 +15,10 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks() throws IOException {
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("/Users/aungbong/Documents/GitHub/markdown-parser/test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals(List.of("https//something.com","some-thing.htlm"), links);
+        assertEquals(List.of("https://something.com","some-thing.html"), links);
 
-        
     }
 }
